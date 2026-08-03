bb6224e8e95e48098a185922a5358fb163851694
AshishResolute <anshugourh21@gmail.com>
2026-08-03 13:56:42 +0530

    Completed with Home section of portfolio

--- portfolio/src/components/main.jsx
+++ portfolio/src/components/main.jsx
@@ -1,7 +1,7 @@
 export const Hero = ({ userName }) => {
   return (
     // use flex-1 so that this div take the remaining space after nav as min-h-screen added a scroll bar
-    <div className=" flex flex-col flex-1  md:flex-row md:items-center   text-[#6b6b68] p-8 overflow-y-auto">
+    <div className=" flex flex-col   md:flex-row md:items-center   text-[#6b6b68] p-8 overflow-y-auto">
       <div className="space-y-3  p-4">
         <h2>Hey,I'm {userName}</h2>
         <h1 className="text-[#d85a30] text-3xl">
@@ -25,7 +25,7 @@ export const Hero = ({ userName }) => {
 
 const CodeCard = () => {
   return (
-    <div className="border border-black flex-1">
+    <div className="border border-slate-200 bg-black rounded-2xl flex-1">
       <div className="flex gap-2 p-4 ">
         <span className="rounded-[50%] bg-red-300 inline-block w-2 h-2 "></span>
         <span className="rounded-[50%] bg-yellow-300 inline-block w-2 h-2"></span>
@@ -34,16 +34,17 @@ const CodeCard = () => {
       <div>
         <pre className="font-mono px-3">
           {`POST /api/auth/login
-{   
-  "email": "user@bank.com",
-  "password": "••••••••"
-}
-200 Ok
-{
-  "token": "eyJhbGciOi...",
-  "expiresIn": 3600
-}
-            `}
+  {   
+    "email": "user@social.com",
+    "password": "••••••••"
+  }\n`}
+          <span className="text-green-400"> 200 OK</span>
+          {`
+  {
+    "token": "eyJhbGciOi...",
+    "expiresIn": 3600
+  }
+  `}
         </pre>
       </div>
     </div>
