693960d2e5b1adac9055400c739a892a9ed52dbd
AshishResolute <anshugourh21@gmail.com>
2026-08-07 20:53:48 +0530

    Added form on right of contact section,needs better styling and functionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,12 +5,24 @@ import { contactDetails } from "../data/projects.data.js";
 
 export const Contact = () => {
   return (
-    <div className=" text-[#6b6b68] space-y-4 p-8 pt-6">
-      <ContactSrcDivHeader />
-      <ContactInlineLinks />
+    <div className=" text-[#6b6b68]  p-8 pt-6 grid grid-cols-1 md:grid-cols-2 gap-6 border border-white">
+      <ContactDivBtns/>
+      <ContactForm/>
     </div>
   );
 };
+
+const ContactDivBtns = ()=>{
+  return (
+    <div className="space-y-4">
+      <ContactSrcDivHeader />
+      <ContactInlineLinks />
+    </div>
+  )
+}
+
+
+
 
 const ContactSrcDivHeader = () => {
   return (
@@ -29,9 +41,25 @@ const ContactSrcDivHeader = () => {
 const ContactInlineLinks = () => {
   return (
     <div className=" space-y-3">
-      <a href={contactDetails.email} className="flex p-2 items-center gap-4 border border-white " target="_blank"><HiOutlineMail/> {contactDetails.email}</a>
-      <a href={contactDetails.github} className="flex p-2 items-center gap-4 border border-white " target="_blank"><FiGithub/> github/ashishResolute</a>
-      <a href={contactDetails.linkedin} className="flex p-2 items-center gap-4 border border-white " target="_blank"><FaLinkedinIn/> linkedin/ashishgourh</a>
+      <a href={contactDetails.email} className="flex p-2 items-center gap-4 border border-slate-500 " target="_blank"><HiOutlineMail/> {contactDetails.email}</a>
+      <a href={contactDetails.github} className="flex p-2 items-center gap-4 border border-slate-500 " target="_blank"><FiGithub/> github/ashishResolute</a>
+      <a href={contactDetails.linkedin} className="flex p-2 items-center gap-4 border border-slate-500 " target="_blank"><FaLinkedinIn/> linkedin/ashishgourh</a>
     </div>
   );
 };
+
+const ContactForm = ()=>{
+  return(
+    <div className="border border-white p-8">
+      <form action="" className="flex flex-col">
+        <label htmlFor="name">Name</label>
+        <input type="text" />
+        <label htmlFor="email">Email</label>
+        <input type="email" />
+        <label htmlFor="message">Message</label>
+        <textarea name="message" id="message">What's on your mind?</textarea>
+        <input type="submit" />
+      </form>
+    </div>
+  )
+}
